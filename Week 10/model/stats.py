9b2c59bce38156a2863a3f828e8d20713abd3a05
Iqbal Ramadhan Anniswa <95537097+kayabaakihiko13@users.noreply.github.com>
2023-09-09 23:41:02 +0700

    Update stats.py

--- Week 10/model/stats.py
+++ Week 10/model/stats.py
@@ -1,12 +1,15 @@
 from model.math import *
 from random import random
-def random_uniform(low,high,size=None):
-    if size==None:
-        return low+(high-low)*random()
-    if size<0:
-        raise ValueError('size not in real number on negative')
-    memo=[]# memory
+def random_uniform(low : int|float,
+                   high : int|float,
+                   size:int=None):
+    if size == None:
+        return low + (high - low) * random()
+    if size < 0:
+        raise ValueError("size can't be 0 or blank")
+    # this is for saving value iterable
+    memo :list =[]
     for i in range(size):
-        result=low+(high-low)*random()
+        result =low +(high -low) * random()
         memo.append(result)
-    return (memo)    
+    return memo    
